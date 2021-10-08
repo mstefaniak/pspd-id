def625cc824e6bcf489fa8fb6d3ef204044db71b
krzysztof.janiec@punkta.pl <krzysztof.janiec@mbp-punkta.home>
2021-10-08 19:58:08 +0200

    ugly fix

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,7 +1,7 @@
 export const LOCALE = 'pl-PL'
 export const API_URL = 'https://api.pspd.online'
-export const APP_VERSION = 'APP_VERSION'
-export const API_VERSION = 'API_VERSION'
+export const APP_VERSION = '00000'
+export const API_VERSION = '11111'
 // export const API_URL = 'http://localhost:4000'
 export const OUTDATE_DIFF = 1000 * 60 * 60 * 8 // 8 godzin
 
